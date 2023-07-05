de576ed71693c5bcb3f5a01ded06f1b1eb160b5f
Trenzen <103641280+itsTrenzen@users.noreply.github.com>
2023-07-06 00:23:53 +0200

    Quicksave

--- script.js
+++ script.js
@@ -62,6 +62,8 @@ function setMode(mode) {
         allItems.forEach(item => { 
             item.classList.remove("hide");    
         });
+        p1Item1_1.classList.add("setP1move");
+        p2Item1_1.classList.add("setP2move");
     }
     gameMode = mode;
 }
@@ -130,6 +132,27 @@ function setMove(p, n) { //p for player and n for the item selected
         }
     }
 }
+//function takes item from player
+function takeItem(p, item) {
+    if (p == "p1") {
+        for (let index = 0; index < player1.length; index++) {
+            if (player1[index] != null && player1[index] == item) {
+                player1[index] == null; 
+                return true;
+                } 
+            }
+            return false;
+        } else {
+        for (let index = 0; index < player2.length; index++) {
+            if (player2[index] != null && player2[index] == item) {
+                player2[index] == null; 
+                return true;
+                } 
+            }
+            return false;
+    }
+}
+
 //firstly animate the clicked tile and then log it into the system
 function colorTile(tile) {
     if (gameActive) {
@@ -137,60 +160,141 @@ function colorTile(tile) {
         statusBarP1.classList.add("hide"); //animating statusbars
         statusBarP2.classList.remove("hide");
         switch (tile) {
-            case "f1":
-                if (f1.classList != "tileClickedP1") {
+            case "f1":        
+                if (gameMode == "nor") {
+                    currentGridP1[0] = 1;
+                    if (f1.classList != "tileClickedP1") {
                     f1.classList.add("tileClickedP1");
+                    }
+                } else {
+                    currentGridP1[0] = currentMoveP1;
+                    if (takeItem("p1", currentMoveP1)) {
+                        if (f1.classList != "tileClickedP1") {
+                            f1.classList.add("tileClickedP1");
+                            }
+                    }
                 }
-                currentGridP1[0] = 1;
-                break;
-        
+                break;   
             case "f2":
-                if (f2.classList != "tileClickedP1") {
+                
+                if (gameMode == "nor") {
+                    currentGridP1[1] = 1;
+                    if (f2.classList != "tileClickedP1") {
                     f2.classList.add("tileClickedP1");
-                }
-                currentGridP1[1] = 1;
+                    }
+                } else {
+                    currentGridP1[0] = currentMoveP1;
+                    takeItem("p1", currentMoveP1);
+                    if (f2.classList != "tileClickedP1") {
+                        f2.classList.add("tileClickedP1");
+                        }
+                }                
                 break;
             case "f3":
+                
+                if (gameMode == "nor") {
+                currentGridP1[2] = 1;
                 if (f3.classList != "tileClickedP1") {
                     f3.classList.add("tileClickedP1");
-                }
-                currentGridP1[2] = 1;
+                    }
+                } else {
+                currentGridP1[0] = currentMoveP1;
+                takeItem("p1", currentMoveP1);
+                if (f3.classList != "tileClickedP1") {
+                    f3.classList.add("tileClickedP1");
+                    }
+                }    
                 break;
             case "f4":
+                
+                if (gameMode == "nor") {
+                currentGridP1[3] = 1;
                 if (f4.classList != "tileClickedP1") {
                     f4.classList.add("tileClickedP1");
                 }
-                currentGridP1[3] = 1;
+                } else {
+                currentGridP1[0] = currentMoveP1;
+                takeItem("p1", currentMoveP1);
+                if (f4.classList != "tileClickedP1") {
+                    f4.classList.add("tileClickedP1");
+                    }
+                } 
                 break;
             case "f5":
+               
+                if (gameMode == "nor") {
+                currentGridP1[4] = 1;
                 if (f5.classList != "tileClickedP1") {
                     f5.classList.add("tileClickedP1");
+                    }
+                } else {
+                currentGridP1[0] = currentMoveP1;
+                takeItem("p1", currentMoveP1);
+                if (f5.classList != "tileClickedP1") {
+                    f5.classList.add("tileClickedP1");
+                    }
                 }
-                currentGridP1[4] = 1;
                 break;
             case "f6":
+                
+                
+                if (gameMode == "nor") {
+                currentGridP1[5] = 1;
                 if (f6.classList != "tileClickedP1") {
                     f6.classList.add("tileClickedP1");
                 }
-                currentGridP1[5] = 1;
+                } else {
+                currentGridP1[0] = currentMoveP1;
+                takeItem("p1", currentMoveP1);
+                if (f6.classList != "tileClickedP1") {
+                    f6.classList.add("tileClickedP1");
+                    }
+                }
                 break;
             case "f7":
+                
+                if (gameMode == "nor") {
+                currentGridP1[6] = 1;
                 if (f7.classList != "tileClickedP1") {
                     f7.classList.add("tileClickedP1");
+                    }
+                } else {
+                currentGridP1[0] = currentMoveP1;
+                takeItem("p1", currentMoveP1);
+                if (f7.classList != "tileClickedP1") {
+                    f7.classList.add("tileClickedP1");
+                    }
                 }
-                currentGridP1[6] = 1;
                 break;
             case "f8":
+                
+                if (gameMode == "nor") {
+                currentGridP1[7] = 1;
                 if (f8.classList != "tileClickedP1") {
                     f8.classList.add("tileClickedP1");
+                    }
+                } else {
+                currentGridP1[0] = currentMoveP1;
+                takeItem("p1", currentMoveP1);
+                if (f8.classList != "tileClickedP1") {
+                    f8.classList.add("tileClickedP1");
+                    }
                 }
-                currentGridP1[7] = 1;
                 break;
             case "f9":
+                
+                if (gameMode == "nor") {
+                currentGridP1[8] = 1;
                 if (f9.classList != "tileClickedP1") {
                     f9.classList.add("tileClickedP1");
+                    }
+                } else {
+                currentGridP1[0] = currentMoveP1;
+                takeItem("p1", currentMoveP1);
+                if (f9.classList != "tileClickedP1") {
+                    f9.classList.add("tileClickedP1");
+                    }
                 }
-                currentGridP1[8] = 1;
                 break;
         }
         isTurn = false;
@@ -199,59 +303,140 @@ function colorTile(tile) {
         statusBarP1.classList.remove("hide");
         switch (tile) {
             case "f1":
+                
+                if (gameMode == "nor") {
+                currentGridP2[0] = 1;
                 if (f1.classList != "tileClickedP2") {
                     f1.classList.add("tileClickedP2");
+                    }
+                } else {
+                currentGridP2[0] = currentMoveP2;
+                takeItem("p2", currentMoveP2);
+                if (f1.classList != "tileClickedP2") {
+                    f1.classList.add("tileClickedP2");
+                    }
                 }
-                currentGridP2[0] = 1;
                 break;
         
             case "f2":
+                
+                if (gameMode == "nor") {
+                currentGridP2[1] = 1;
                 if (f2.classList != "tileClickedP2") {
                     f2.classList.add("tileClickedP2");
+                    }
+                } else {
+                currentGridP2[0] = currentMoveP2;
+                takeItem("p2", currentMoveP2);
+                if (f2.classList != "tileClickedP2") {
+                    f2.classList.add("tileClickedP2");
+                    }
                 }
-                currentGridP2[1] = 1;
                 break;
             case "f3":
+                
+                if (gameMode == "nor") {
+                currentGridP2[2] = 1;
                 if (f3.classList != "tileClickedP2") {
                     f3.classList.add("tileClickedP2");
+                    }
+                } else {
+                currentGridP2[0] = currentMoveP2;
+                takeItem("p2", currentMoveP2);
+                if (f3.classList != "tileClickedP2") {
+                    f3.classList.add("tileClickedP2");
+                    }
                 }
-                currentGridP2[2] = 1;
                 break;
             case "f4":
+                
+                if (gameMode == "nor") {
+                currentGridP2[3] = 1;
                 if (f4.classList != "tileClickedP2") {
                     f4.classList.add("tileClickedP2");
+                    }
+                } else {
+                currentGridP2[0] = currentMoveP2;
+                takeItem("p2", currentMoveP2);
+                if (f4.classList != "tileClickedP2") {
+                    f4.classList.add("tileClickedP2");
+                    }
                 }
-                currentGridP2[3] = 1;
                 break;
             case "f5":
+                
+                if (gameMode == "nor") {
+                currentGridP2[4] = 1;
+                if (f5.classList != "tileClickedP2") {
+                    f5.classList.add("tileClickedP2");
+                    }
+                } else {
+                currentGridP2[0] = currentMoveP2;
+                takeItem("p2", currentMoveP2);
                 if (f5.classList != "tileClickedP2") {
                     f5.classList.add("tileClickedP2");
                 }
-                currentGridP2[4] = 1;
+                }
                 break;
             case "f6":
+                
+                if (gameMode == "nor") {
+                currentGridP2[5] = 1;
                 if (f6.classList != "tileClickedP2") {
                     f6.classList.add("tileClickedP2");
+                    }
+                } else {
+                currentGridP2[0] = currentMoveP2;
+                takeItem("p2", currentMoveP2);
+                if (f6.classList != "tileClickedP2") {
+                    f6.classList.add("tileClickedP2");
+                    }
                 }
-                currentGridP2[5] = 1;
                 break;
             case "f7":
+                
+                if (gameMode == "nor") {
+                currentGridP2[6] = 1;
                 if (f7.classList != "tileClickedP2") {
                     f7.classList.add("tileClickedP2");
+                    }
+                } else {
+                currentGridP2[0] = currentMoveP2;
+                takeItem("p2", currentMoveP2);
+                if (f7.classList != "tileClickedP2") {
+                    f7.classList.add("tileClickedP2");
+                    }
                 }
-                currentGridP2[6] = 1;
                 break;
             case "f8":
+                
+                if (gameMode == "nor") {
+                currentGridP2[7] = 1;
                 if (f8.classList != "tileClickedP2") {
                     f8.classList.add("tileClickedP2");
+                    }
+                } else {
+                currentGridP2[0] = currentMoveP2;
+                takeItem("p2", currentMoveP2);
+                if (f8.classList != "tileClickedP2") {
+                    f8.classList.add("tileClickedP2");
+                    }
                 }
-                currentGridP2[7] = 1;
                 break;
             case "f9":
+              
+                if (gameMode == "nor") {
+                currentGridP2[8] = 1;
                 if (f9.classList != "tileClickedP2") {
                     f9.classList.add("tileClickedP2");
+                    }
+                } else {
+                currentGridP2[0] = currentMoveP2;
+                takeItem("p2", currentMoveP2);
+                if (f9.classList != "tileClickedP2") {
+                    f9.classList.add("tileClickedP2");
+                    }
                 }
-                currentGridP2[8] = 1;
                 break;
             }
         isTurn = true;
@@ -334,7 +519,9 @@ function checkWinNormalP2(grid) {
 }
 //Checks win for advanced mode
 function checkWinAdvP1() {
-    let check1, check2, check3;
+    let check1 = 1;
+    let check2 = 1; 
+    let check3 = 1;
      
     for (let i = 0; i<=8; i++) {
         if (currentGridP1[check1] != 0 && currentGridP1[check2] != 0 && currentGridP1[check3] != 0) {
@@ -359,7 +546,31 @@ function checkWinAdvP1() {
     }
 }
 function checkWinAdvP2() {
-
+    let check1 = 1;
+    let check2 = 1; 
+    let check3 = 1;
+     
+    for (let i = 0; i<=8; i++) {
+        if (currentGridP2[check1] != 0 && currentGridP2[check2] != 0 && currentGridP2[check3] != 0) {
+            return true;
+        } else if (i == 0) {
+            check1 = 0; check2 = 1; check3 = 2;
+        } else if (i == 1) {
+            check1 = 3; check2 = 4; check3 = 5;
+        } else if (i == 2) {
+            check1 = 6; check2 = 7; check3 = 8;
+        } else if (i == 3) {
+            check1 = 0; check2 = 3; check3 = 6;
+        } else if (i == 4) {
+            check1 = 1; check2 = 4; check3 = 7;
+        } else if (i == 5) {
+            check1 = 2; check2 = 5; check3 = 8;
+        } else if (i == 6) {
+            check1, check2, check3 = 0, 4, 8;
+        } else if (i == 7) {
+            check1, check2, check3 = 2, 4, 6;
+        }
+    }
 }
 
 function reset() {
